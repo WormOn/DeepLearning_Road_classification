867468c2dc7e876368c3f279a5aa5542577f7b5c
aditya7041 <Aditya@adityas-MacBook-Pro-2.local>
2016-11-09 18:12:06 -0800

    Adding support for feeding input images

--- basic_cnn.py
+++ basic_cnn.py
@@ -1,36 +1,82 @@
-
-'''
-A Convolutional Network implementation example using TensorFlow library.
-This example is using the MNIST database of handwritten digits
-(http://yann.lecun.com/exdb/mnist/)
-Author: Aymeric Damien
-Project: https://github.com/aymericdamien/TensorFlow-Examples/
-'''
-
 from __future__ import print_function
 
 import tensorflow as tf
+import os
+import numpy as np
 
 # Import MNIST data
 from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
+#mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
+
+# Learn about the mnist input data :
+#batch_x, batch_y = mnist.train.next_batch(5)
+#x_shape = batch_x.shape
+#print(batch_x[1:])
+#for i in range(x_shape[1]):
+#    if(batch_x[1][i]>0.0):
+#        break
+
+#print(batch_x.shape)
+#print(batch_y.shape)
+
+# input our images into numpy arrays
+print(os.getcwd())
+
+def get_data_and_label():
+    
+    # step 1
+    filenames = ['images/8.png']
+    
+    # step 2
+    filename_queue = tf.train.string_input_producer(filenames)
+    
+    # step 3: read, decode and resize images
+    reader = tf.WholeFileReader()
+    filename, content = reader.read(filename_queue)
+    image = tf.image.decode_png(content, channels=1)
+    image = tf.cast(image, tf.float32)
+    print(image)
+    image = tf.image.resize_images(image, [28, 28])
+    print(image)
+    
+    # step 4: Batching
+    image_batch = tf.train.batch([image], batch_size=1)
+    
+    data = np.array(image)
+    label = np.array([0,1])
+    
+    result = []
+    result.append(data)
+    result.append(label)
+    
+    return result
+
+#data = tf.image.decode_png("images/8.png",channels=3)
+#grey_image = tf.image.rgb_to_grayscale(data)
+#resized_image = grey_image
+#resized_image = tf.image.resize_images(grey_image, [28, 28])
+#print("resized_image =",resized_image)
+#resized_image = tf.cast(resized_image, tf.float32)
+#print(" resized_image = ",resized_image)
+#data = np.multiply(resized_image, 1.0 / 255.0)
+#print("normalized images =",data.dtype)
+#print(data)
 
 # Parameters
 learning_rate = 0.001
-training_iters = 200000
-batch_size = 128
+training_iters = 2000
+batch_size = 1
 display_step = 10
 
 # Network Parameters
 n_input = 784 # MNIST data input (img shape: 28*28)
-n_classes = 10 # MNIST total classes (0-9 digits)
-dropout = 0.75 # Dropout, probability to keep units
+n_classes = 2 # MNIST total classes (0-9 digits)
+dropout = 0.00 # Dropout, probability to keep units
 
 # tf Graph input
 x = tf.placeholder(tf.float32, [None, n_input])
 y = tf.placeholder(tf.float32, [None, n_classes])
 keep_prob = tf.placeholder(tf.float32) #dropout (keep probability)
-
 
 # Create some wrappers for simplicity
 def conv2d(x, W, b, strides=1):
@@ -39,39 +85,45 @@ def conv2d(x, W, b, strides=1):
     x = tf.nn.bias_add(x, b)
     return tf.nn.relu(x)
 
-
 def maxpool2d(x, k=2):
     # MaxPool2D wrapper
     return tf.nn.max_pool(x, ksize=[1, k, k, 1], strides=[1, k, k, 1],
                           padding='SAME')
 
-
 # Create model
 def conv_net(x, weights, biases, dropout):
     # Reshape input picture
+    print('Executing conv_net')
     x = tf.reshape(x, shape=[-1, 28, 28, 1])
-
+    
     # Convolution Layer
     conv1 = conv2d(x, weights['wc1'], biases['bc1'])
     # Max Pooling (down-sampling)
     conv1 = maxpool2d(conv1, k=2)
-
+    print('Done with Conv1 and MaxPool1 layer execution')
+    
     # Convolution Layer
     conv2 = conv2d(conv1, weights['wc2'], biases['bc2'])
     # Max Pooling (down-sampling)
     conv2 = maxpool2d(conv2, k=2)
-
+    print('Done with Conv2 and MaxPool2 layer execution')
+    
     # Fully connected layer
     # Reshape conv2 output to fit fully connected layer input
     fc1 = tf.reshape(conv2, [-1, weights['wd1'].get_shape().as_list()[0]])
     fc1 = tf.add(tf.matmul(fc1, weights['wd1']), biases['bd1'])
     fc1 = tf.nn.relu(fc1)
+    print('Done with fc1 execution')
+    
     # Apply Dropout
     fc1 = tf.nn.dropout(fc1, dropout)
-
+    
     # Output, class prediction
     out = tf.add(tf.matmul(fc1, weights['out']), biases['out'])
     return out
+
+def print_step(step):
+    print("step = ",step)
 
 # Store layers weight & bias
 weights = {
@@ -111,24 +163,26 @@ with tf.Session() as sess:
     sess.run(init)
     step = 1
     # Keep training until reach max iterations
-    while step * batch_size < training_iters:
-        batch_x, batch_y = mnist.train.next_batch(batch_size)
-        # Run optimization op (backprop)
-        sess.run(optimizer, feed_dict={x: batch_x, y: batch_y,
-                                       keep_prob: dropout})
+    #while step * batch_size < training_iters:
+    while step < 10:
+        #while 1==0:
+        result = get_data_and_label()
+        batch_x = result[0]
+        batch_y = result[1]
+        print("before running optimizer : ",step)
+        sess.run(optimizer, feed_dict={x: batch_x, y: batch_y})
+        print_step(step)
         if step % display_step == 0:
             # Calculate batch loss and accuracy
-            loss, acc = sess.run([cost, accuracy], feed_dict={x: batch_x,
-                                                              y: batch_y,
-                                                              keep_prob: 1.})
+            loss, acc = sess.run([cost, accuracy], feed_dict={x: batch_x, y: batch_y,keep_prob: 1.})
             print("Iter " + str(step*batch_size) + ", Minibatch Loss= " + \
                   "{:.6f}".format(loss) + ", Training Accuracy= " + \
                   "{:.5f}".format(acc))
         step += 1
     print("Optimization Finished!")
 
-    # Calculate accuracy for 256 mnist test images
-    print("Testing Accuracy:", \
-        sess.run(accuracy, feed_dict={x: mnist.test.images[:256],
-                                      y: mnist.test.labels[:256],
-                                      keep_prob: 1.}))
+# Calculate accuracy for 256 mnist test images
+#print("Testing Accuracy:", \
+#    sess.run(accuracy, feed_dict={x: mnist.test.images[:256],
+#                                 y: mnist.test.labels[:256],
+#
